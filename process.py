57ffdbb467f60c13c8afc1e69d4e028d35855e8e
senapol <sensen.zp@gmail.com>
2025-02-17 17:36:40 +0000

    added modes and config

--- process.py
+++ process.py
@@ -1,10 +1,75 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', type=str, default='constant', help='Mode for processing data: constant or percapita')
+args = parser.parse_args()
+print(args.mode)
+
+countries_of_interest = {
+    'direct': ['Ukraine', 'Russia'],
+    'neighbouring': ['Belarus', 'Moldova', 'Poland', 'Romania', 'Hungary', 'Slovakia'],
+    'baltic': ['Estonia', 'Latvia', 'Lithuania'],
+    'other': ['Germany', 'France', 'United Kingdom', 'United States of America', 'Finland']
+}
+
+def filter_relevant_countries(df, countries):
+    'Filter df to only include countries of interest'
+
+    df = df[df['Country'].isin(countries)]
+    return df
 
 def process_milex_excel(path):
     'Read and convert Milex xlsx file to pandas df'
 
-    df = pd.read_excel(path, sheet_name='Constant (2022) US$', skiprows=5)
+    # Variables based on data mode
+    if args['mode'] == 'constant':
+        sheet_name = 'Constant (2022) US$'
+        skiprows = 5
+        ylabel = 'Military Expenditure in Millions (Constant 2022 US$) '
+    elif args['mode'] == 'percapita':
+        sheet_name = 'Per capita'
+        skiprows = 6
+        ylabel = 'Military Expenditure per capita in Millions (Current US$)'
 
+    try:
+        print(f'Reading file: {path}')
+        df = pd.read_excel(path, sheet_name=sheet_name, skiprows=skiprows, na_values=['...', ''])
+
+        print(df)
+        return df
+
+    except Exception as e:
+        print(f'Error {e}: Could not read file')
+        return None
+
+def clean_df(df):
     
+        # Systematically clean df
+        df = df.drop(columns=[col for col in df.columns if ('Unnamed' in str(col) or 'Notes' in str(col))])
+        df = df.dropna(subset=['Country'])
+
+        # Filter to only include countries of interest
+
+        df = filter_relevant_countries(df, countries_of_interest['direct'] + countries_of_interest['neighbouring'] + countries_of_interest['baltic'] + countries_of_interest['other'])
+        return df
+
+df = process_milex_excel('SIPRI-Milex-data-1948-2023.xlsx')
+print(df.columns[df.isna().all()])
+
+df_ukruss = filter_relevant_countries(df, countries_of_interest['direct'])
+df_neighbours = filter_relevant_countries(df, countries_of_interest['neighbouring'])
+df_baltic = filter_relevant_countries(df, countries_of_interest['baltic'])
+df_other = filter_relevant_countries(df, countries_of_interest['other'])
+
+year_columns = [col for col in df.columns if str(col).isnumeric() and pd.to_numeric(col) >= 2010]
+
+df = df.melt(id_vars = ['Country'], value_vars=year_columns, var_name='Year', value_name='Milex')
+
+df = df[df['Country'] != 'United States of America']
+
+fig = px.line(df, x='Year', y='Milex', title='Military Expenditure over Time', color='Country', labels={'Milex': ylabel, 'Year': 'Year'})
+fig.show()
+
